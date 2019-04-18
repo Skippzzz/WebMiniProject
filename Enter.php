607da5b487f9d8e97450f09f266ea73e06f0c931
Anders Skipper <asje15@student.aau.dk>
2019-04-18 10:22:50 +0200

    commit

---
+++ Enter.php
@@ -0,0 +1,26 @@
+<?php include('login.php')?>
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <title>Login</title>
+     <link rel="stylesheet" href="style.css">
+   </head>
+   <body>
+     <form method="post" action="Enter.php" id="login_form">
+  <h1>Login</h1>
+  <div <?php if (isset($login_error)): ?> class="form_error" <?php endif ?> >
+  <input type="text" name="username" placeholder="Username" required="required" value="<?php echo $username; ?>">
+  <?php if (isset($login_error)): ?>
+    <span><?php echo $login_error; ?></span>
+  <?php endif ?>
+  </div>
+  <div>
+    <input type="password"  placeholder="Password" name="password" required="required">
+  </div>
+  <div>
+      <button type="submit" name="login" id="login_btn">Login</button>
+      <button type="button" name="login" id="reg_btn" onclick="location.href='http://localhost/webProjects/PhpLogin/WebMiniProject/registering.php'">Register</button>
+  </div>
+</form>
+   </body>
+ </html>
---
+++ register.php
@@ -0,0 +1,36 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'twsm');
+// Open connection
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+  $username = "";
+  if (isset($_POST['register'])) {
+    $username = mysqli_real_escape_string($db,$_POST['username']);
+    $password = mysqli_real_escape_string($db,$_POST['password']);
+
+  	$sql_u = "SELECT * FROM user WHERE username='$username'";
+    $sql_p = "SELECT * FROM user WHERE password='$password'";
+  	$res_u = mysqli_query($db, $sql_u);
+    $res_p = mysqli_query($db, $sql_p);
+
+    $user = mysqli_fetch_assoc($res_u);
+
+  	if ($user) {
+       if ($user['username'] === $username) {
+         $name_error = "Sorry... username already taken";
+              // echo "not welcome";
+      }
+  	}else{
+           $query = "INSERT INTO user (username, password)
+      	    	  VALUES ('$username', '$password')";
+           $results = mysqli_query($db, $query);
+           // echo "welcome";
+           header("Location: http://localhost/webProjects/PhpLogin/WebMiniProject/Portfolio.html");
+           exit();
+  	}
+  }
+  mysqli_close($db);
+?>
