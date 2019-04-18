13228ceca57c08e1df554cd092cd48573b9a4cde
Anders Skipper <asje15@student.aau.dk>
2019-04-18 12:34:52 +0200

    Ajax plz work

--- Ajax.js
+++ Ajax.js
@@ -1,10 +1,9 @@
 $('nav a').on('click', function(e) {                 // User clicks nav link
   e.preventDefault();                                // Stop loading new link
-  var url = $(this).attr('href');                    // Get value of href
-  var $content = $('#content');
+  var url = this.href;                               // Get value of href
   $('nav a.current').removeClass('current');         // Clear current indicator
   $(this).addClass('current');                       // New current indicator
 
   $('#container').remove();                          // Remove old content
-  $('#content').load(url + '#container');            // New content
+  $('#content').load(url + ' #container').hide().fadeIn('slow');            // New content
 });
