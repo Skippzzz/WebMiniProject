06d7429a417f528d43c1eeda354da6ad7b4ce6b5
Unknown <mskr16@student.aau.dk>
2019-04-18 11:57:45 +0200

    Meh

--- Ajax.js
+++ Ajax.js
@@ -1,9 +1,10 @@
 $('nav a').on('click', function(e) {                 // User clicks nav link
   e.preventDefault();                                // Stop loading new link
   var url = $(this).attr('href');                    // Get value of href
+  var $content = $('#content');
   $('nav a.current').removeClass('current');         // Clear current indicator
   $(this).addClass('current');                       // New current indicator
 
   $('#container').remove();                          // Remove old content
-  $('#content').load(url + '#container');            // New content
+  $(content).load(url + '#container');            // New content
 });
